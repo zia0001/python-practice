f3c1613f2503de6b219485097e49ae11de0f9df7
zia ud din <ziawc0001@gmail.com>
2025-07-16 18:12:05 +0500

    exercise 3 weight conversion

--- main.py
+++ main.py
@@ -3,6 +3,15 @@
 # is_new = True
 
 
-name = input('What is your name: ')
-favourite_color = input('What is your favourite color: ')
-print(name,'likes',favourite_color)
+# name = input('What is your name: ')
+# favourite_color = input('What is your favourite color: ')
+# print(name,'likes',favourite_color)
+
+# birth_year = int(input('Birth year: '))
+# age = 2025 - birth_year
+# print(age)
+
+
+weight_lb = int(input('what is your weight(lb): '))
+weight_kg = weight_lb * 0.453592
+print(weight_kg,'kg')
