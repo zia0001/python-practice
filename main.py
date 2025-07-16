104ab2fcb4530e420343e33c04e4e63240804e96
zia ud din <ziawc0001@gmail.com>
2025-07-16 17:49:28 +0500

    exercise 2 input method

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-full_patient_name = 'John Smith'
-age = 20
-is_new = True
+# full_patient_name = 'John Smith'
+# age = 20
+# is_new = True
 
 
+name = input('What is your name: ')
+favourite_color = input('What is your favourite color: ')
+print(name,'likes',favourite_color)
