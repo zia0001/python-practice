914240ad717ec8b28c2afa8ec9dd9c2019caf502
zia ud din <ziawc0001@gmail.com>
2025-07-17 22:44:08 +0500

    Exercise 6 if-else

--- main.py
+++ main.py
@@ -1,61 +1,62 @@
 import math
-# #
-# # full_patient_name = 'John Smith'
-# # age = 20
-# # is_new = True
-# #
-# #
-# # name = input('What is your name: ')
-# # favourite_color = input('What is your favourite color: ')
-# # print(name,'likes',favourite_color)
-# #
-# # birth_year = int(input('Birth year: '))
-# # age = 2025 - birth_year
-# # print(age)
-# #
-# #
-# # weight_lb = int(input('what is your weight(lb): '))
-# # weight_kg = weight_lb * 0.453592
-# # print(weight_kg,'kg')
-# #
-# #
-# # message = 'deployed successfully'
-# # alert = message
-# # print(alert[0:5])
-# #
-# #
-# # name = 'Jennifer'
-# # print(name[1:-1])
-# #
-# #
-# # course = 'learning mathematics'
-# # print(len(course), course.upper())
-# # print(course.find('tics'))         # 7
-# # print(course.replace('atics','atata')) #replaces characters
-# #
-# # print('learning' in course) # returns bool value
-# #
-# #
-# # print(10 // 3)   #return integer i.e 3 without floating points
-# # print(10 % 4)    #returns remainder
-# # print(3 ** 3)    #raise to power
-# #
-# # x = 10
-# # x = x + 4
-# # x += 4            #augmented assignment operator--- for shorter code
-# # print(x)
-# #
-# #
-# # x = 10 + 6 * 3 ** 2
-# # print(x)            #return 64---following Expo>division>multi>add>sub
-# # y = (5 + 3) * 6 ** 2 / 2
-# # print(y)            #return 144.o following BEDMAS
-# #
-# #
-# # y = 78.56
-# # print(round(y))
-# # print(abs(-988.00))         #gives absolute (+ive) value
-# # print(math.factorial(5))
+
+
+full_patient_name = 'John Smith'
+age = 20
+is_new = True
+
+
+name = input('What is your name: ')
+favourite_color = input('What is your favourite color: ')
+print(name,'likes',favourite_color)
+
+birth_year = int(input('Birth year: '))
+age = 2025 - birth_year
+print(age)
+
+
+weight_lb = int(input('what is your weight(lb): '))
+weight_kg = weight_lb * 0.453592
+print(weight_kg,'kg')
+
+
+message = 'deployed successfully'
+alert = message
+print(alert[0:5])
+
+
+name = 'Jennifer'
+print(name[1:-1])
+
+
+course = 'learning mathematics'
+print(len(course), course.upper())
+print(course.find('tics'))         # 7
+print(course.replace('atics','atata')) #replaces characters
+
+print('learning' in course) # returns bool value
+
+
+print(10 // 3)   #return integer i.e 3 without floating points
+print(10 % 4)    #returns remainder
+print(3 ** 3)    #raise to power
+
+x = 10
+x = x + 4
+x += 4            #augmented assignment operator--- for shorter code
+print(x)
+
+
+x = 10 + 6 * 3 ** 2
+print(x)            #return 64---following Expo>division>multi>add>sub
+y = (5 + 3) * 6 ** 2 / 2
+print(y)            #return 144.o following BEDMAS
+
+
+y = 78.56
+print(round(y))
+print(abs(-988.00))         #gives absolute (+ive) value
+print(math.factorial(5))
 
 
 is_hot = False
@@ -70,6 +71,17 @@ else:
     print("It's a lovely day")
 
 print("Enjoy your day")
+
+house_price = 1000000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = house_price * 0.1
+else:
+    down_payment = house_price * 0.2
+print(f"down_payment: ${down_payment}")
+
+
 
 
 
