1ebc8696ffe6f430b30f1fd38acc70037107365a
zia ud din <ziawc0001@gmail.com>
2025-07-17 16:06:02 +0500

    exercise 4

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-# full_patient_name = 'John Smith'
-# age = 20
-# is_new = True
+full_patient_name = 'John Smith'
+age = 20
+is_new = True
 
 
-# name = input('What is your name: ')
-# favourite_color = input('What is your favourite color: ')
-# print(name,'likes',favourite_color)
+name = input('What is your name: ')
+favourite_color = input('What is your favourite color: ')
+print(name,'likes',favourite_color)
 
-# birth_year = int(input('Birth year: '))
-# age = 2025 - birth_year
-# print(age)
+birth_year = int(input('Birth year: '))
+age = 2025 - birth_year
+print(age)
 
 
 weight_lb = int(input('what is your weight(lb): '))
 weight_kg = weight_lb * 0.453592
 print(weight_kg,'kg')
+
+
+message = 'deployed successfully'
+alert = message
+print(alert[0:5])
+
+
+name = 'Jennifer'
+print(name[1:-1])
