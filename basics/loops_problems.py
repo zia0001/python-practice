1f802234f626edb4ae9574361d4b130de4de1f68
zia ud din <ziawc0001@gmail.com>
2025-08-03 15:49:38 +0500

    code uncommented

--- basics/loops_problems.py
+++ basics/loops_problems.py
@@ -1,16 +1,16 @@
-#-------count positive numbers----------
+# -------count positive numbers----------
 
 
 numbers = [1, -2, 3, -4, 5, -6, -7, -9, 8, 10]
 positive_numbers = []
 
-# for number  in  numbers:
-#   if number > 0:
-#     positive_numbers.append(number)
-# print(positive_numbers)
+for number  in  numbers:
+  if number > 0:
+    positive_numbers.append(number)
+print(positive_numbers)
 
 
-#----------Sum of even numbers-------
+# ----------Sum of even numbers-------
 
 numbers = [2,5,1,7,41,62,88,73,54]
 even_numbers = []
