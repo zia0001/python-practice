e53d4df70d5b4b117b05d1441d59c0b41c869948
zia ud din <ziawc0001@gmail.com>
2025-08-08 23:45:31 +0500

    file modifiec with another class

--- basics/oop/inheritance.py
+++ basics/oop/inheritance.py
@@ -16,3 +16,20 @@ c2 = Car("Mehran", 2025)
 
 print(c1.full_name())
 print(c2.full_name())
+
+class ElectricCar(Car):
+  def __init__(self, brand, model, battery_size):
+    super().__init__(brand, model)
+    self.battery_size = battery_size
+
+  def full_name(self):
+    return f"{self.brand} : {self.model} : Battery {self.battery_size}"
+  
+
+  # def __str__(self):
+  #   return f"{self.full_name()} with battery {self.battery_size}"
+
+
+
+c3 = ElectricCar('Toyota',2025,'24F')
+print(c3.full_name())
