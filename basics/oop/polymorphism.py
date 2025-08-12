2fbe82e3421e43d8fb8284e6ebcf820e89173f34
zia ud din <ziawc0001@gmail.com>
2025-08-12 19:41:18 +0500

    counts total cars added

--- basics/oop/polymorphism.py
+++ basics/oop/polymorphism.py
@@ -1,14 +1,14 @@
 class Car:
-
+  total_cars = 0
   def __init__(self, brand, model):
     self.brand = brand
     self.model = model
-
+    Car.total_cars += 1
   def fuel_type(self):
     return "Petrol or Diesel or CNG"
     
 
-c1= Car('pijaro', 2021)
+#c1= Car('pijaro', 2021)
 
 
 class ElectricCar(Car):
@@ -16,8 +16,8 @@ class ElectricCar(Car):
   def fuel_type(self):     #common method but different usage
     return "Electric charge"
   
-Ecar = ElectricCar('Tesla', 2025)
+Ecar = ElectricCar('TATA', 2025)
 print(f"{Ecar.brand} {Ecar.model} {Ecar.fuel_type()}")
 
-print(Ecar)
+print(Car.total_cars)
 
